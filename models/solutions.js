9d283bcf952a2726b48331ee8f804c7ea2c64ef2
rakesh.k <rakesh.k@pacewisdom.com>
2021-08-02 17:12:19 +0530

    createdOn and license object added to the model

--- models/solutions.js
+++ models/solutions.js
@@ -86,6 +86,8 @@ module.exports = {
         default : "Domains",
         type : String
       },
-      criteriaLevelReport : Boolean
+      criteriaLevelReport : Boolean,
+      license:Object,
+      creatorName:String
     }
   };
